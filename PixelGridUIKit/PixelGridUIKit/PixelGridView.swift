a5955b0f0ecff49fcda4f4314a70851a236ca837
Ole Begemann <ole@oleb.net>
2014-11-22 20:10:28 +0100

    Document PixelGridView

--- PixelGridUIKit/PixelGridUIKit/PixelGridView.swift
+++ PixelGridUIKit/PixelGridUIKit/PixelGridView.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 enum RenderingMode {
-    case LogicalPixels
-    case NativePixels
+    case LogicalPixels // Render to the "logical" pixel resolution (i.e. 3x on the iPhone 6 Plus)
+    case NativePixels  // Try to render to "hardware" pixel resolution (i.e. with screen.nativeScale on the iPhone 6 Plus)
 }
 
 @objc protocol PixelGridViewDelegate: NSObjectProtocol {
@@ -47,24 +47,29 @@ class PixelGridView: UIView {
         }
     }
 
+    // For 1-pixel lines, line width should be 1.0/scaleFactor points
     var lineWidth: CGFloat {
         return 1.0/renderScaleFactor
     }
 
+    // The bounds rect in (logical or hardware) pixels, depending on the rendering mode
     var pixelRect: CGRect {
         let pixelRect = CGRectApplyAffineTransform(self.bounds, upscaleTransform)
         return pixelRect
     }
-    
+
+    // Scales from points to pixels
     var upscaleTransform: CGAffineTransform {
         let scaleFactor = renderScaleFactor
         return CGAffineTransformMakeScale(scaleFactor, scaleFactor)
     }
     
+    // Scales from pixels to points
     var downscaleTransform: CGAffineTransform {
         return CGAffineTransformInvert(upscaleTransform)
     }
     
+    // Renders the view contents into an image of size pixelRect pixels with lineWidth = 1 px
     func renderToImage() -> UIImage {
         let path = pixelGridPath()
         path.lineWidth = 1.0
@@ -78,6 +83,13 @@ class PixelGridView: UIView {
         return image
     }
     
+    // Constructs the pixel grid that the view should draw.
+    // For convenience, the path returned by this method uses the pixelRect as its coordinate system
+    // (i.e. 1242x2208 (logical pixels) or 1080x1920 (native pixels) on the iPhone 6 Plus).
+    // This means we can construct the path by iterating over whole pixels and use a line offset of 0.5
+    // to align lines on whole pixels.
+    // Before drawing, the path returned by this method gets scaled down to the view's coordinate system
+    // with downscaleTransform.
     func pixelGridPath() -> UIBezierPath {
         let path = UIBezierPath()
         
@@ -85,7 +97,10 @@ class PixelGridView: UIView {
         
         var step: CGFloat = 2.0
         var i = 0
+
+        // Comment out one of these options for the actual drawing:
         
+        // Option 1: draw a grid of vertical lines with varying spacing
         var x = CGRectGetMinX(pixelRect)
         while x <= CGRectGetMaxX(pixelRect) {
             let lineCenterX = x + lineOffset
@@ -100,22 +115,25 @@ class PixelGridView: UIView {
             }
             x += step
         }
+        // End option 1
         
-        //        for x in stride(from: CGRectGetMinX(pixelRect), through: CGRectGetMaxX(pixelRect), by: 2) {
-        //            let lineCenterX = x + 0.5
-        //            let startPoint = CGPoint(x: lineCenterX, y: CGRectGetMinY(pixelRect))
-        //            let endPoint = CGPoint(x: lineCenterX, y: CGRectGetMaxY(pixelRect))
-        //            path.moveToPoint(startPoint)
-        //            path.addLineToPoint(endPoint)
-        //        }
-        //
-        //        for y in stride(from: CGRectGetMinY(pixelRect), through: CGRectGetMaxY(pixelRect), by: 2) {
-        //            let lineCenterY = y + 0.5
-        //            let startPoint = CGPoint(x: CGRectGetMinX(pixelRect), y: lineCenterY)
-        //            let endPoint = CGPoint(x: CGRectGetMaxX(pixelRect), y: lineCenterY)
-        //            path.moveToPoint(startPoint)
-        //            path.addLineToPoint(endPoint)
-        //        }
+        // Option 2: draw a grid of vertical lines with 1-pixel spacing in between
+//        for x in stride(from: CGRectGetMinX(pixelRect), through: CGRectGetMaxX(pixelRect), by: 2) {
+//            let lineCenterX = x + lineOffset
+//            let startPoint = CGPoint(x: lineCenterX, y: CGRectGetMinY(pixelRect))
+//            let endPoint = CGPoint(x: lineCenterX, y: CGRectGetMaxY(pixelRect))
+//            path.moveToPoint(startPoint)
+//            path.addLineToPoint(endPoint)
+//        }
+//
+//        for y in stride(from: CGRectGetMinY(pixelRect), through: CGRectGetMaxY(pixelRect), by: 2) {
+//            let lineCenterY = y + lineOffset
+//            let startPoint = CGPoint(x: CGRectGetMinX(pixelRect), y: lineCenterY)
+//            let endPoint = CGPoint(x: CGRectGetMaxX(pixelRect), y: lineCenterY)
+//            path.moveToPoint(startPoint)
+//            path.addLineToPoint(endPoint)
+//        }
+        // End option 2
         
         return path
     }
